777337967abbc9355bd232c6cf4f4d68436d2cd5
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2022-07-21 21:58:43 -0400

    added ESC keypress event

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Dev Kitty
-Personal project #1: A fun place for some dev knowledge! HTML, CSS, JS
+# Right-Meow
+Personal project #1: Cat Fortune Teller/Magic Eight Ball, for the days when you just can't make a decision! HTML, CSS, JS
--- main.js
+++ main.js
@@ -14,22 +14,31 @@
 //   alert(answer);
 // }
 
-"use strict";
+("use strict");
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("button clicked!");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 }
 
-btnCloseModal.addEventListener("click", function () {
+function closeModal() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
 });
