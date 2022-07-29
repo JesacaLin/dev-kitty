578518bc655e66c883f96f8970cb3086d35ae196
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2022-07-28 20:42:18 -0400

    added eventListener to overlay

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener('click', closeModal)
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
