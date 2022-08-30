fbfc24f3c7b133c948e7e0bd6c646f1253f42811
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2022-08-29 22:59:36 -0400

    moved questions to data.js file
    Linked it to html
    Added more questions

---
+++ data.js
@@ -0,0 +1,95 @@
+
+//Exporting this data to main.js
+
+
+//LOOK ---> TIPS
+
+const behavorTips = [
+    "Give me an example of the project or initiative that you started on your own. What prompted you to get started?",
+    "Tell me about a time you had to work on several projects at once. How did you handle this?",
+    "Describe a situation in which you felt you had not communicated well enough. How did you handle it?",
+    "Tell me about when you had to deal with conflict within your team. How was the conflict solved? Would you do anything differently now?",
+    "Give me an example of a time you had to take a creative and unusual approach to solve coding problem. How did this idea come to your mind? Why do you think it was unusual?",
+    "Describe a situation in which you worked diligently on a project and it did not produce the desired results. Why didn't you get the desired results? What did you learn from the experience?",
+    "Give an example of an important project goal you reached and how you achieved it.",
+    "Describe a situation in which you experienced difficulty in getting others to accept your ideas? What was your approach? How did this work? Were you able to successfully persuade someone to see things your way",
+    "Tell me about a situation when you were responsible for project planning. Did everything go according to your plan? If not, then why and what kind of counteractions did you have to take",
+    "Tell me about a situation when you made a mistake at work. What happened exactly and how did you deal with it? What steps did you take to improve the situation?",
+    "Tell me about a time when you worked with someone who was not completing his or her share of the work. How did you handle the situation? Did you discuss your concern with your coworker? With your manager? If yes, how did your coworker respond to your concern? What was your manager's response?",
+    "Describe a situation when you worked effectively under pressure. How did you feel when working under pressure? What was going on, and how did you get through it?",
+    "Tell me about yourself.",
+    "Tell me about your experience at ____ bootcamp.",
+    "What do you know about our company?",
+
+  ];
+  
+  const htmlTips = [
+    "What does a doctype do?",
+    "How do you serve a page with content in multiple languages?",
+    "What kind of things must you be wary of when design or developing for multilingual sites?",
+    "What are data- attributes good for?",
+    "Consider HTML5 as an open web platform. What are the building blocks of HTML5?",
+    "Describe the difference between a cookie, sessionStorage and localStorage.",
+    "Describe the difference between <script>, <script async> and <script defer>.",
+    "Why is it generally a good idea to position CSS <link>s between <head></head> and JS <script>s just before </body>? Do you know any exceptions?",
+    "What is progressive rendering?",
+    "Why you would use a srcset attribute in an image tag? Explain the process the browser uses when evaluating the content of this attribute.",
+    "Have you used different HTML templating languages before?",
+  ];
+  
+  const cssTips = [
+    "What is CSS selector specificity and how does it work?",
+    "What's the difference between 'resetting' and 'normalizing' CSS? Which would you choose, and why?",
+    "Describe z-index and how stacking context is formed.",
+    "Describe BFC (Block Formatting Context) and how it works.",
+    "What are the various clearing techniques and which is appropriate for what context?",
+    "Explain CSS sprites, and how you would implement them on a page or site.",
+    "How would you approach fixing browser-specific styling issues?",
+    "How do you serve your pages for feature-constrained browsers? What techniques/processes do you use?",
+    "What are the different ways to visually hide content (and make it available only for screen readers)?",
+    "Have you ever used a grid system, and if so, what do you prefer?",
+    "Have you used or implemented media queries or mobile specific layouts/CSS?",
+    "Are you familiar with styling SVG?",
+    "Can you give an example of an @media property other than screen?",
+    "What are some of the \"gotchas\" for writing efficient CSS?",
+  ];
+  
+  const jsTips = [
+    "Explain how event delegation works in JavaScript.",
+    "Explain how prototypal inheritance works.",
+    "What do you think of AMD vs CommonJS?",
+    "Explain why the following doesn't work as an IIFE: function foo(){ }();. What needs to be changed to properly make it an IIFE?",
+    "What's the difference between a variable that is: null, undefined or undeclared? How would you go about checking for any of these states?",
+    "What is a closure, and how/why would you use one?",
+    "Can you describe the main difference between a .forEach loop and a .map() loop and why you would pick one versus the other?",
+    "What's a typical use case for anonymous functions?",
+    "How do you organize your code? (module pattern, classical inheritance?)",
+    "What's the difference between host objects and native objects?",
+    "Difference between: function Person(){}, var person = Person(), and var person = new Person()?",
+    "What's the difference between .call and .apply?",
+    "Explain Function.prototype.bind.",
+    "When would you use document.write()?",
+    "What's the difference between feature detection, feature inference, and using the UA string?",
+  ];
+  
+  const nodeTips = [
+    "What is Node.js? Where can you use it?",
+    "Why use Node.js?",
+    "What are the features of Node.js?",
+    "How do you update NPM to a new version in Node.js?",
+    "Why is Node.js Single-threaded?",
+    "Explain callback in Node.js",
+    "What is callback hell in Node.js?",
+    "How do you prevent/fix callback hell?",
+    "Explain the role of REPL in Node.js",
+    "Name the types of API functions in Node.js.",
+    "What are the functionalities of NPM in Node.js?",
+    "What is the difference between Node.js and Ajax?",
+    "What are “streams” in Node.js? Explain the different types of streams present in Node.js.",
+    "Explain chaining in Node.js.",
+    "What are Globals in Node.js?",
+  ];
+  
+  const surpriseTips = ["...Coming Soon!", "...Even more coming soon!", "...Let me ask Reddit"];
+  
+
--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 "use strict";
 
 //TODO
-//Will export array data to another document in the future
-//need to refactor tips functions into one multi-use one...
 //figure out how prevent the same tip from displaying back to back
+
+
+//LOOK --->IMPORTING DATA FROM ANOTHER FILE USING MODULES.
+// Interestingly we can import array data from another document by using modules. It doesn't seem to work within the main.js file but it will work directly in the html file. Had to use import vs required as require only works on server side...https://tinyurl.com/2p8pxzya
+// import { behavorTips } from "./data.js";
+// import { htmlTips } from "./data.js";
+
 
 //LOOK ---> modal behavior
 const modal = document.querySelector(".modal");
@@ -115,45 +120,5 @@ function getNodeTips() {
   moreModalFunc(getNodeTips);
 }
 
-//LOOK ---> TIPS
 
-const behavorTips = [
-  "Give me an example of the project or initiative that you started on your own. What prompted you to get started?",
-  "Tell me about a time you had to work on several projects at once. How did you handle this?",
-  "Describe a situation in which you felt you had not communicated well enough. How did you handle it?",
-  "Tell me about when you had to deal with conflict within your team. How was the conflict solved? Would you do anything differently now?",
-  "Give me an example of a time you had to take a creative and unusual approach to solve coding problem. How did this idea come to your mind? Why do you think it was unusual?",
-];
 
-const htmlTips = [
-  "What does a doctype do?",
-  "How do you serve a page with content in multiple languages?",
-  "What kind of things must you be wary of when design or developing for multilingual sites?",
-  "What are data- attributes good for?",
-  "Consider HTML5 as an open web platform. What are the building blocks of HTML5?",
-];
-
-const cssTips = [
-  "What is CSS selector specificity and how does it work?",
-  "What's the difference between 'resetting' and 'normalizing' CSS? Which would you choose, and why?",
-  "Describe z-index and how stacking context is formed.",
-  "Describe BFC (Block Formatting Context) and how it works.",
-];
-
-const jsTips = [
-  "Explain how event delegation works in JavaScript.",
-  "Explain how prototypal inheritance works.",
-  "What do you think of AMD vs CommonJS?",
-  "Explain why the following doesn't work as an IIFE: function foo(){ }();. What needs to be changed to properly make it an IIFE?",
-  "What's the difference between a variable that is: null, undefined or undeclared? How would you go about checking for any of these states?",
-];
-
-const nodeTips = [
-  "What is Node.js? Where can you use it?",
-  "Why use Node.js?",
-  "What are the features of Node.js?",
-  "How do you update NPM to a new version in Node.js?",
-  "Why is Node.js Single-threaded?",
-];
-
-const surpriseTips = ["...Coming Soon!", "...Even more coming soon!"];
